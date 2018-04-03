3c48c212b5022eed5aa9beb561d93215bf73d588
Kelsey Fecho <kfecho30@gmail.com>
2018-04-03 23:45:55 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,8 +2,4 @@ class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
   belongs_to :network
-
-  def build_network(args)
-    self.network = network_id.name
-  end
 end
