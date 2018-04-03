def430f655b93703d5f8d3ec587a5d06667f410f
Kelsey Fecho <kfecho30@gmail.com>
2018-04-03 23:32:00 +0000

    automatically backed up by learn

--- db/migrate/007_data_to_shows.rb
+++ db/migrate/007_data_to_shows.rb
@@ -1,4 +1,4 @@
-class AddDataToShows < ActiveRecord::Migration[5.1]
+class DataToShows < ActiveRecord::Migration[5.1]
   def change
     add_column :shows, :day, :string
     add_column :shows, :day, :string
